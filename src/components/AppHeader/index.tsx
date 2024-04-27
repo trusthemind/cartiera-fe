1477986f64301de8edf382870ac794a95b36e69b
Fedir Melnyk <pluserwork24@gmail.com>
2024-04-27 14:54:51 +0300

    base settup

---
+++ src/components/AppHeader/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+import s from "./style.module.scss";
+import { AppRoutes, NavigationList, Theme } from "@/src/constants/constants";
+import Link from "next/link";
+import Avatar from "antd/es/avatar";
+import { ThemeSwitcher } from "../ThemeSwitcher";
+import cn from "classnames";
+import { useCurrentPathEqual } from "@/src/helpers/pathEqual";
+import { UserOutlined } from "@ant-design/icons";
+
+
+export const AppHeader = () => {
+  const { isEqual:IsHome } = useCurrentPathEqual(AppRoutes.Home);
+console.log(AppRoutes.Details)
+
+  return (
+    <header className={s.headerContainer}>
+      <h1 className={cn(s.textLogo, { [s.textDarkLogo]: false })}>Cartiera Sales</h1>
+      <ul className={s.navigationList}>
+        {NavigationList.map((item, index) => (
+          <li key={index}>
+            <Link href={item.ref}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className={s.authControlContainer}>
+        {!IsHome && <ThemeSwitcher />}
+        <Avatar style={{backgroundColor:"var(--black)"}} icon={<UserOutlined />}/>
+      </div>
+    </header>
+  );
+};
