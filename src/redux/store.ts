a0c74a4e6c04f7cf7329f54302b5d15522ff970d
Fedir Melnyk <pluserwork24@gmail.com>
2024-06-01 10:39:41 +0300

    .

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, configureStore, } from "@reduxjs/toolkit";
 import { authSlice } from "./slices/auth";
 import { api } from "../api";
 import storage from "redux-persist/lib/storage";
@@ -6,23 +6,22 @@ import { persistReducer } from "redux-persist";
 
 //! persist checker for store
 
-
 const authConfig = {
-  key:"auth",
+  key: "auth",
   storage,
-  whiteList:["token"].
-}
+  whiteList: ["token"],
+};
 
-const  persitedAuth = persistReducer(authConfig,authSlice.reducer)
+const persitedAuth = persistReducer(authConfig, authSlice.reducer);
 
 const store = configureStore({
-  reducer: { [authSlice.name]: authSlice.reducer, auth: persitedAuth },
+  reducer: { auth: persitedAuth },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat([api.middleware]),
+    }).concat([api.middleware]) as any,
 });
 
 export default store;
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
